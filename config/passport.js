0132b43852a8711763add31cbc0f785375580c73
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-05-20 15:33:47 -0400

    add passport authenication route to express router

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,6 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/user");
 
-// ...
-
 passport.use(
   "signup",
   new LocalStrategy(
@@ -24,3 +22,43 @@ passport.use(
     },
   ),
 );
+
+// ...
+
+passport.use(
+  "login",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user, { message: "Login sucessful" });
+      } catch (error) {
+        return done(error);
+      }
+    },
+  ),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const passport = require("passport");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.post(
+  "sign-up",
+  passport.authenticate("signup", { session: false }),
+  async function (req, res, next) {
+    res.json({ message: "Sign up sucessful", user: req.user });
+  },
+);
 
 module.exports = router;
